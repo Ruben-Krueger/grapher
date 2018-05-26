4d152c8e7eedf220abf78819fab013fd91ff6191
Ruben Krueger <rubenkrueger99@gmail.com>
2018-05-25 23:32:44 -0700

    new

--- data.js
+++ data.js
@@ -1,35 +1,6 @@
  /**
  * Created by rubenkrueger on 5/25/18.
  */
- function Component(type, name, x, y, result, color, critical) {
-     this.type = type;
-     this.name = name;
-     this.x = x;
-     this.y = y;
-     this.color = color;
-     this.radius = 0.5;
-     this.vulnerability = result;
-     this.critical = critical;
-
- }
-
- var switches = [];
- var servers = [];
- var PLCs = [];
- var engines = [];
-
- var n_switches = [];
- var n_servers = [];
- var n_PLCs = [];
- var n_engines = [];
-
- var switch1 = new Component(Switch, "switch 1", 100, 100, "Unknown", "black", "False");
- var switch2 = new Component(Switch, "switch 2", 200, 200, "Unknown", "black", "False");
- var switch3 = new Component(Switch, "switch 3", 300, 300, "Unknown", "black", "False");
- var switch4 = new Component(Switch, "switch 4", 400, 100, "Unknown", "black", "False");
- var switch5 = new Component(Switch, "switch 5", 100, 100, "Unknown", "black", "False");
-
-
 
  // for (i = 1; i <= n_switches; i++)
  //     g.nodes.push({
--- graph.js
+++ graph.js
@@ -1,11 +1,48 @@
 /**
  * Created by rubenkrueger on 5/25/18.
  */
+var i,
+    s,
+    g = {
+        nodes: [],
+        edges: []
+    };
+
+function Component(type, id, x, y, result, color, critical) {
+    this.label = id;
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.size = 0.5;
+    this.vulnerability = result;
+    this.critical = critical;
+
+}
 
 
+var switch1 = new Component("Switch", "Switch 1", 100, 100, "Unknown", "black", "False");
+var switch2 = new Component("Switch", "Switch 2", 200, 100, "Unknown", "black", "False");
+var switch3 = new Component("Switch", "Switch 3", 200, 200, "Unknown", "black", "False");
+
+var generator1 = new Component("Generator", "Generator 1", 250, 150, "Unknown", "chocolate", "True");
+var generator2 = new Component("Generator", "Generator 2", 250, 300, "Unknown", "chocolate", "False");
+
+var plc1 = new Component("PLC", "PLC 1", 75, 50, "Unknown", "darkcyan", "False");
+var plc2 = new Component("PLC", "PLC 2", 100, 150, "Unknown", "darkcyan", "False");
+var plc3 = new Component("PLC", "PLC 3", 50, 200, "Unknown", "darkcyan", "False");
+var plc4 = new Component("PLC", "PLC 4", 80, 300, "Unknown", "darkcyan", "False");
 
 
-
+g.nodes.push(switch1);
+g.nodes.push(switch2);
+g.nodes.push(switch3);
+g.nodes.push(generator1);
+g.nodes.push(generator2);
+g.nodes.push(plc1);
+g.nodes.push(plc2);
+g.nodes.push(plc3);
+g.nodes.push(plc4);
 
 s = new sigma({
     graph: g,
